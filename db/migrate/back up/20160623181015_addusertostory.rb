740e9a534da8deb1630622dd8fa66cb6eb3500b2
ramyalzayat <ramy.za.rr@gmail.com>
2016-09-06 20:31:54 +0200

    fixingdatabasedrop

--- db/migrate/back up/20160623181015_addusertostory.rb
+++
@@ -1,6 +0,0 @@
-class Addusertostory < ActiveRecord::Migration
-  def change
-  	add_column(:story_models, :name , :string)
-  	add_column(:story_models, :email , :string )
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -16,20 +16,6 @@ ActiveRecord::Schema.define(version: 20160901182358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bootsy_image_galleries", force: :cascade do |t|
-    t.integer  "bootsy_resource_id"
-    t.string   "bootsy_resource_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "bootsy_images", force: :cascade do |t|
-    t.string   "image_file"
-    t.integer  "image_gallery_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.integer  "location"
@@ -63,15 +49,6 @@ ActiveRecord::Schema.define(version: 20160901182358) do
     t.string   "gender"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-  end
-
-  create_table "story_models", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "name"
-    t.string   "email"
   end
 
 end
