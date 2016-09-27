e5daf41bb4c120904c27e09a2b61f9a1a0c9dd21
mhajali <medosmooth@gmail.com>
2016-09-27 21:14:49 +0200

    adding img to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :location, :date, :text, :photos, :privacy, :bus, :car, :boat, :plane, :walking, :train)
+      params.require(:post).permit(:title, :location, :date, :text, :photos, :privacy, :bus, :car, :boat, :plane, :walking, :train, :image)
     end
 end
